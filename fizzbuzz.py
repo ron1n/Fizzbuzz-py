9245c0891841c01dc0639f1ce609424439351ba7
Salvatore Nuzzo <snuzzo@logicworks.net>
2022-04-02 13:06:55 -0400

    with while loop

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,1 +1,16 @@
+def main():
+    i = 0
+    print("let's fizzbuzz")
+    while i < 100:
+        i += 1
+        if i % 3 == 0:
+            print(str(i) + " Fizz")
+            if i % 5 == 0:
+                print(str(i) + " FizzBuzz")
+        elif i % 5 == 0:
+            print(str(i) + " Buzz")
+
+
+if __name__ == "__main__":
+        main()
 
